c339813953296b55dd4850fb88a1fc9b8a9d35e8
G3B3 <gentilboulet@gmail.com>
2017-03-20 23:12:47 +0000

    Basic program options handling.

--- src/programmator_main.cpp
+++ src/programmator_main.cpp
@@ -1,7 +1,53 @@
+#include "boost/program_options.hpp"
+
 #include <iostream>
- 
-int main()
+#include <string>
+
+namespace 
 {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+    const int SUCCESS = 0;
+    const int ERROR_IN_COMMAND_LINE  = 1;
+    const int ERROR_UNHANDLED_EXCEPTION = 2;
+}
+
+int main(int argc, char** argv) 
+{
+    try 
+    {
+        namespace po = boost::program_options; 
+
+        po::options_description cmd_line("Command line options");
+        cmd_line.add_options() 
+            ("help,h", "Print help messages") 
+            ("verbose,v", "print words with verbosity") 
+            ;
+
+        po::variables_map vm;
+        try
+        {
+            po::store(po::parse_command_line(argc, argv , cmd_line), vm);
+
+            if ( vm.count("help")  ) 
+            { 
+                std::cout << cmd_line << std::endl;
+                return SUCCESS; 
+            } 
+
+            po::notify(vm); 
+        }    
+        catch(po::error& e) 
+        { 
+            std::cerr << "ERROR  : " << e.what() << std::endl << std::endl; 
+            std::cerr << cmd_line << std::endl; 
+            return ERROR_IN_COMMAND_LINE; 
+        } 
+    }
+    catch(std::exception& e) 
+    { 
+        std::cerr << "Unhandled Exception reached the top of main: " 
+            << e.what() << ", application will now exit" << std::endl; 
+        return ERROR_UNHANDLED_EXCEPTION; 
+
+    } 
+    return SUCCESS;
 }
